757b738c9d345273ad05b5d6f3cc239c758140c9
habibur-01 <h.r.habibur08@gmail.com>
2024-01-07 13:17:03 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+require('dotenv').config()
+const cors = require('cors')
+const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = "mongodb+srv://thesis01:HE8IXRkSy7si4lF0@cluster0.cbqlcas.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+
+        const dataCollection = client.db('thesisProject').collection('dataset')
+
+        app.get('/data', async (req, res) => {
+            const cursor = dataCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.get('/data/:id', async(req, res) => {
+            const id = req.params.id
+            const query = {_id: new ObjectId(id)}
+            const result= await dataCollection.findOne(query)
+            res.send(result)
+          })
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
